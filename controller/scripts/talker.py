682588f277b4d0c42f69c1902a502735b0857796
bogdan-paulis <bogdan.paulis@tum.de>
2017-05-01 00:35:37 +0200

    Add files via upload
    update speech

--- controller/scripts/talker.py
+++ controller/scripts/talker.py
@@ -10,18 +10,24 @@ from roboy_comm.srv import TextSpoken
 from random import randint
 
 
+from roboy_comm.srv import Talk
+from roboy_comm.srv import seq2seq_predict
+
+
 def talker():
     pub = rospy.Publisher('/new_action', Int8MultiArray, queue_size=10)
     rospy.init_node('talker', anonymous=True)
-    new_act = Int8MultiArray()
-    new_act.layout.dim = []
-    new_act.data=[1,1,2,0]
-    pub.publish(new_act)
 
     rate = rospy.Rate(0.5) # publish every 1/rate seconds
 
     rospy.wait_for_service("TextSpoken")
+    rospy.wait_for_service("/speech_synthesis/talk")
+    rospy.wait_for_service("/roboy/gnlp_predict")
+    
     stt = rospy.ServiceProxy('TextSpoken', TextSpoken)
+    talk = rospy.ServiceProxy('/speech_synthesis/talk', Talk)
+    respond = rospy.ServiceProxy('/roboy/gnlp_predict', seq2seq_predict)
+
 
     random=0
     while not rospy.is_shutdown():
@@ -41,40 +47,56 @@ def talker():
 		rospy.loginfo('roboy heared a one, %r', new_act.data[3])
 		#import pdb;pdb.set_trace()	        
 		pub.publish(new_act)
+		text="You chose one"
 		random=0
 	elif "two" in resp.text: 
 		new_act.data=[1,1,2,1]
 	        pub.publish(new_act)
+		text="You chose two"
 		random=0
 	elif "three" in resp.text: 
 		new_act.data=[1,1,2,2]
 	        pub.publish(new_act)
+		text="You chose three"
 		random=0
 	elif "four" in resp.text: 
 		new_act.data=[1,1,2,3]
 	        pub.publish(new_act)
+		text="You chose four"
 		random=0
 	elif "five" in resp.text: 
 		new_act.data=[1,1,2,4]
 	        pub.publish(new_act)
+		text="You chose five"
 		random=0
 	elif "six" in resp.text: 
 		new_act.data=[1,1,2,5]
 	        pub.publish(new_act)
+		text="You chose six"
 		random=0
 	elif "seven" in resp.text: 
 		new_act.data=[1,1,2,6]
 	        pub.publish(new_act)
+		text="You chose seven"
 		random=0
 	else:
 		random=random+1
 
-	if random==3:
+	if random==4:
 		new_act.data=[1,1,2,randint(0,6)]
 	        pub.publish(new_act)
+		text="Random number"
 		random=0
 	elif random>0:
-		text="What?"
+		text="What did you say?"
+
+	if "profanity" in resp.text: 
+		resp2="Profanity! Profanity Profanity!"
+	else:
+		resp2 =respond(text+" "+resp.text).text_output		
+	print resp2
+	talk(resp2)
+	
 
 #	rospy.spin()
         rate.sleep()
