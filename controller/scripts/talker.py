ea553714f3839cbeba0048f94aa44ab119e6a261
bogdan-paulis <bogdan.paulis@tum.de>
2017-05-01 02:19:59 +0200

    Add files via upload
    final

--- controller/scripts/talker.py
+++ controller/scripts/talker.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # license removed for brevity
 import rospy
+import time
 from std_msgs.msg import String
-
 from std_msgs.msg import Int8MultiArray
 
 from roboy_comm.srv import TextSpoken
@@ -12,6 +12,11 @@ from random import randint
 
 from roboy_comm.srv import Talk
 from roboy_comm.srv import seq2seq_predict
+
+from roboy_comm.srv import ShowEmotion
+
+from roboy_comm.srv import Yaw
+from roboy_comm.srv import Movement
 
 
 def talker():
@@ -23,11 +28,20 @@ def talker():
     rospy.wait_for_service("TextSpoken")
     rospy.wait_for_service("/speech_synthesis/talk")
     rospy.wait_for_service("/roboy/gnlp_predict")
+    rospy.wait_for_service("/roboy_face/show_emotion")
+    rospy.wait_for_service("/roboy_move/yaw")
     
     stt = rospy.ServiceProxy('TextSpoken', TextSpoken)
     talk = rospy.ServiceProxy('/speech_synthesis/talk', Talk)
     respond = rospy.ServiceProxy('/roboy/gnlp_predict', seq2seq_predict)
+    emo = rospy.ServiceProxy('/roboy_face/show_emotion', ShowEmotion)
+    head = rospy.ServiceProxy('/roboy_move/yaw', Yaw)
 
+    talk("It's time to duel!")
+    emo("smileblink")
+    print "It's time to duel!"
+
+    respond_data=["That's a smart move","I see your strategy","Yo","Yo", "You won't win like this", "Hurry up, it's not chess", "Move it grandma", "Even a baby would know not to do this move", "Ha ha","Are you sure you want to do that move?"]
 
     random=0
     while not rospy.is_shutdown():
@@ -85,17 +99,25 @@ def talker():
 	if random==4:
 		new_act.data=[1,1,2,randint(0,6)]
 	        pub.publish(new_act)
-		text="Random number"
+		text="I guess you chose "+str(new_act.data[3]+1)
 		random=0
 	elif random>0:
-		text="What did you say?"
+		text="What did you say? "+respond(resp.text).text_output
+	else:
+		text=text+". "+respond_data[randint(0,len(respond_data)-1)]
 
 	if "profanity" in resp.text: 
-		resp2="Profanity! Profanity Profanity!"
-	else:
-		resp2 =respond(text+" "+resp.text).text_output		
-	print resp2
-	talk(resp2)
+		text="Profanity! Profanity Profanity! Watch your language! "+text
+                head(10)
+		time.sleep(2)
+                head(-10)
+		time.sleep(2)
+                head(0)
+		time.sleep(2)
+
+		random=0	
+	print text
+	talk(text)
 	
 
 #	rospy.spin()
